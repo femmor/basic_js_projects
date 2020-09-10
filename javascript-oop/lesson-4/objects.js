ccf55014c24c660fcf8af8f526e65e8bb8370524
Emmanuel Egomson <fegomson@gmail.com>
2020-09-10 17:26:26 -0500

    class method chaining

--- javascript-oop/lesson-4/objects.js
+++ javascript-oop/lesson-4/objects.js
@@ -1,18 +1,27 @@
 
 class User {
     //  constructor
-    constructor(name, email) {
+    constructor(name, email, score) {
         this.name = name;
-        this.email = email
+        this.email = email;
+        this.score = 0;
     }
 
     //  class methods
     login() {
         console.log(this.name, "just logged in")
+        return this;
     }
 
     logout() {
         console.log(this.name, "just logged out")
+        return this;
+    }
+
+    updateScore() {
+        this.score++;
+        console.log(this.name, "score is now", this.score);
+        return this;
     }
 }
 
@@ -21,5 +30,7 @@ class User {
 var userOne = new User("femmy", "femmy@gmail.com");
 var userTwo = new User("edwige", "edwige@gmail.com");
 
-userOne.login()
-userTwo.logout()
+// userOne.login()
+// userTwo.logout()
+
+userOne.login().updateScore().updateScore().logout()
---
+++ javascript-oop/lesson-6/objects.js
@@ -0,0 +1,25 @@
+
+class User {
+    //  constructor
+    constructor(name, email) {
+        this.name = name;
+        this.email = email
+    }
+
+    //  class methods
+    login() {
+        console.log(this.name, "just logged in")
+    }
+
+    logout() {
+        console.log(this.name, "just logged out")
+    }
+}
+
+
+
+var userOne = new User("femmy", "femmy@gmail.com");
+var userTwo = new User("edwige", "edwige@gmail.com");
+
+userOne.login()
+userTwo.logout()
